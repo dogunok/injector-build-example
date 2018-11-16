d5eea56fb3c9760b060788b0feaf66e3f5a81868
bercly0b <vvworkmail@yandex.ru>
2018-11-16 09:54:57 +0300

    modified file structure

--- build/script.js
+++ build/script.js
@@ -1,8 +1,22 @@
 "use strict";
 
-var str = 'ES6';
-console.log("Hello ".concat(str, "!"));
+function start() {
+  var qs = function qs(selector) {
+    return document.querySelector(selector);
+  };
 
-var a = function a() {
-  return console.log('--->', 2);
-};
+  var moveFantomasZone = function moveFantomasZone() {
+    var btn = qs('.zone-coordonnees .zone-fantomas');
+    var parent = qs('.zoneCTA');
+    parent.insertAdjacentElement('afterbegin', btn);
+  };
+
+  Kameleoon.API.runWhenElementPresent('.zone-coordonnees .zone-fantomas', moveFantomasZone);
+}
+
+var check_Kameleoon = setInterval(function () {
+  if (typeof Kameleoon !== 'undefined') {
+    clearInterval(check_Kameleoon);
+    start();
+  }
+}, 100);
--- src/script.js
+++ src/script.js
@@ -1,3 +1,34 @@
-const str = 'ES6'
-console.log(`Hello ${str}!`)
-const a = () => console.log('--->', 2)
+function start() {
+
+  const qs = selector => document.querySelector(selector)
+
+  const moveFantomasZone = () => {
+    const btn = qs('.zone-coordonnees .zone-fantomas')
+    const parent = qs('.zoneCTA')
+    parent.insertAdjacentElement('afterbegin', btn)
+  }
+
+  Kameleoon.API.runWhenElementPresent('.zone-coordonnees .zone-fantomas', moveFantomasZone)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var check_Kameleoon = setInterval(function() {
+  if (typeof Kameleoon !== 'undefined') {
+    clearInterval(check_Kameleoon)
+    start()
+  }
+}, 100)
